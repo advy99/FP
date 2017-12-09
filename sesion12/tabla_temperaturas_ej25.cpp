d4aa4a9923f6c1f187c60d8c771de5f419097560
Antonio David Villegas <advy99@gmail.com>
2017-12-09 21:59:40 +0100

    añadido metodo para maximo entre minimos y corregido metodo Minimo

--- sesion12/tabla_temperaturas_ej25.cpp
+++ sesion12/tabla_temperaturas_ej25.cpp
@@ -3,16 +3,23 @@
 
 using namespace std;
 
+struct MaximoTempEntreMinimos{
+   int fila;
+   int columna;
+};
+
 class TablaTemperaturas{
 private:
    static const int NUM_CIUDADES = 10;
    static const int NUM_TEMPERATURAS = 24;
 
+
    double tabla_temperaturas[NUM_CIUDADES][NUM_TEMPERATURAS];
 
-   bool ExisteCiudadYTemperatura(int num_ciudad, int num_temperatura){
+
+   bool ExisteCiudadYTemperatura(int num_ciudad, int num_hora){
       if( num_ciudad >= 0     && num_ciudad < NUM_CIUDADES &&
-          num_temperatura >=0 && num_temperatura < NUM_TEMPERATURAS ){
+          num_hora >=0 && num_hora < NUM_TEMPERATURAS ){
 
          return true;
       }
@@ -21,27 +28,50 @@ private:
    }
 public:
 
-   double Valor(int num_ciudad, int num_temperatura){
-      if( ExisteCiudadYTemperatura(num_ciudad, num_temperatura) )
-         return tabla_temperaturas[num_ciudad][num_temperatura];
+   double Valor(int num_ciudad, int num_hora){
+      if( ExisteCiudadYTemperatura(num_ciudad, num_hora) )
+         return tabla_temperaturas[num_ciudad][num_hora];
       else
          return NAN;
    }
-   void Modifica(int num_ciudad, int num_temperatura, double nueva_temperatura){
-      if( ExisteCiudadYTemperatura(num_ciudad, num_temperatura) ){
-         tabla_temperaturas[num_ciudad][num_temperatura] = nueva_temperatura;
+   void Modifica(int num_ciudad, int num_hora, double nueva_temperatura){
+      if( ExisteCiudadYTemperatura(num_ciudad, num_hora) ){
+         tabla_temperaturas[num_ciudad][num_hora] = nueva_temperatura;
       }
    }
    int Minimo(int fila_a_buscar){
-      int columna_minimo = tabla_temperaturas[fila_a_buscar][0];
+      int columna_minimo = 0;
 
       for(int i = 1; i < NUM_TEMPERATURAS; i++){
-         if(tabla_temperaturas[fila_a_buscar][i] < columna_minimo)
-            columna_minimo = tabla_temperaturas[fila_a_buscar][i];
+         if(tabla_temperaturas[fila_a_buscar][i] < tabla_temperaturas[fila_a_buscar][columna_minimo])
+            columna_minimo = i;
       }
 
       return columna_minimo;
    }
+
+   MaximoTempEntreMinimos MaxMinimos(){
+      MaximoTempEntreMinimos maximo_entre_minimos;
+      int minimos_por_fila[NUM_CIUDADES];
+
+      for (int i = 0; i < NUM_CIUDADES; i++){
+         minimos_por_fila[i] = Minimo(i);
+      }
+
+      maximo_entre_minimos.fila = 0;
+      maximo_entre_minimos.columna = minimos_por_fila[0];
+
+      for(int i = 1; i < NUM_CIUDADES; i++){
+         if(tabla_temperaturas[i][minimos_por_fila[i]] > tabla_temperaturas[maximo_entre_minimos.fila][maximo_entre_minimos.columna]){
+            maximo_entre_minimos.fila = i;
+            maximo_entre_minimos.columna = minimos_por_fila[i];
+         }
+      }
+
+
+      return maximo_entre_minimos;
+   }
+
 };
 
 int main(){
